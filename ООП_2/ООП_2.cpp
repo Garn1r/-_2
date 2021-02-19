509e4463da5293a41ff01582c7611dc9020f9fb6
Garn1r <rahimov0608@mail.ru>
2021-02-19 05:38:37 +0500

    Дабавил новый класс

--- ООП_2/ООП_2.cpp
+++ ООП_2/ООП_2.cpp
@@ -2,9 +2,11 @@
 #include <conio.h>
 #include "stdio.h"
 
+
+
 class student//создание класса student
 {
-//protected:
+	//protected:
 public:
 	int age; //  публичныq атрибут
 public:
@@ -18,7 +20,7 @@ public:
 		printf("student(int age)\n");
 		this->age = age;
 	}
-	student(const student &Luntik)//копирующий конструктор, переносит все свойства из переданного объекта
+	student(const student& Luntik)//копирующий конструктор, переносит все свойства из переданного объекта
 	{
 		printf("student(const point &Luntik)\n");
 		age = Luntik.age;
@@ -39,6 +41,42 @@ void student::vozrast()
 	age = 0;
 }
 
+
+
+class height_student :public student
+{
+protected:
+	int height;
+public:
+	height_student() :student()//конструктор по умолчанию
+	{
+		printf("height_student()\n");//отладочный вывод, чтоб видеть какой конструктор вызвается
+		height = 0; //свойства класса student (поля)   // присваиваем значение свойствам
+	}
+	height_student(int age, int height) :student(age)//конструктор с параметрами
+	{
+		printf("height_student(int age)\n");
+		this->height = height;
+	}
+	height_student(const height_student& Luntik)//копирующий конструктор, переносит все свойства из переданного объекта
+	{
+		printf("height_student(const point &Luntik)\n");
+		height = Luntik.height;
+	}
+	~height_student()//деструктор
+	{
+		printf("%d, height=%d\n", age, height);//вывод текущего состояния объекта, чтоб видеть что в объекте хранилось
+		printf("~height_student()\n\n");
+	}
+	void change_height(int new_height)
+	{
+		height = new_height;
+	}
+	void vozrast();
+};
+
+
+
 int main()
 {
 	{
@@ -46,20 +84,33 @@ int main()
 		student Iskander(19);
 		student Ainur(Iskander);
 	}
-	student *DinamLuntik=new student;//динамически создаём объекты
-	student *DinamIskander=new student(19);
-	student *DinamAinur=new student(*DinamIskander);
+	student* DinamLuntik = new student;//динамически создаём объекты
+	student* DinamIskander = new student(19);
+	student* DinamAinur = new student(*DinamIskander);
 	delete DinamLuntik;
 	delete DinamIskander;
 	delete DinamAinur;
 
-	student *DinamIan = new student(19);
+
+	student* DinamIan = new student(19);
 	DinamIan->vozrast();
 	DinamIan->summ(2);
 	delete DinamIan;
 
 
+	height_student* Iskander = new height_student(19, 180);
+	delete Iskander;
+
+
+	student *Lee = new student(21);
+	//height_student *Vlad = Lee;
+    student *Michael = new height_student(20, 12);
+	height_student *Vlad = new height_student(20, 13);
+	delete Michael;
+	delete Vlad;
+
+
 	_getch();
-	return 0 ;
+	return 0;
 }
 
